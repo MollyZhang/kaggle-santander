ec548d8c903673022ac1320766fe9154d51f5350
Molly Zhang <mollyzhang@ucsc.edu>
2016-04-09 01:05:40 -0700

    add random forest

--- code/Python/machine learning.py
+++ code/Python/machine learning.py
@@ -1,32 +1,32 @@
 import pandas as pd
 import numpy as np
-from sklearn import cross_validation, linear_model, metrics, tree
+from sklearn import cross_validation, linear_model, metrics, tree, ensemble
 
 
-CLASSIFIERS = {"LR": linear_model.LogisticRegression(),
-               "Dtree": tree.DecisionTreeClassifier()}
+CLASSIFIERS = {#"LR": linear_model.LogisticRegression(),
+               "Dtree": tree.DecisionTreeClassifier(max_depth=20),
+               "Forest": ensemble.RandomForestClassifier()}
 
 
 def main():
 
-    print "data loading"
+    print "data loading....."
     df = pd.read_csv("../../data/train.csv")
     data, label = data_label_split(df)
     # data = data[['var38', 'var15']]
     x_train, x_test, y_train, y_test = cross_validation.train_test_split(
         data, label, test_size=0.2, train_size=0.8, random_state=0, stratify=label)
 
-    print "training and prediction"
+    print "training and prediction....."
 
     result_df = tenfold_cross_validation(x_train, y_train, CLASSIFIERS)
-    print result_df.mean
-
+    print result_df.mean()
 
 
 def tenfold_cross_validation(x_train, y_train, classifiers):
     result_df = pd.DataFrame()
     foldnum = 0
-    for train, val in cross_validation.KFold(len(x_train), shuffle=True, n_folds=10, random_state=0):
+    for train, val in cross_validation.StratifiedKFold(y_train, shuffle=True, n_folds=3, random_state=0):
         foldnum += 1
         [tr_data, val_data, tr_targets, val_targets] = folds_to_split(x_train, y_train, train, val)
         tr_targets = tr_targets.as_matrix().ravel()
