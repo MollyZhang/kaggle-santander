3e1432d054d7c62f0ff2a5b83595d62eff5843e9
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2016-04-12 18:42:05 -0400

    added pandas_profiling, repushed Training_data_profiling.ipynb;  moved testTrainSplit.ipynb to testTrainSplit.py so it can be run as a standalone file at the start of new notebooks

---
+++ code/Python/testTrainSplit.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sklearn.cross_validation import train_test_split
+
+df = pd.read_csv("../../data/trainReduced.csv")
+
+# Remove column "Unnamed: 0" (artifact of data write in R)
+df = df.drop(df.columns[[0]], axis = 1)
+
+# Replace IDs with int vals
+df.ix[:, 0] = range(1, len(df) + 1)
+
+
+# Spit into test/train----------------------------
+
+# First split out TARGET column, to confrom to sklearn style
+y = df.ix[:, 'TARGET']
+X = df.drop('TARGET', axis = 1)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size = 0.20, random_state = 1, stratify = y)
