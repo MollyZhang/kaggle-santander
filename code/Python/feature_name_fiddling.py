bcacf2bfe1325a0a9c58f07864bc443dc1471eb9
Molly Zhang <mollyzhang@ucsc.edu>
2016-04-29 15:14:21 -0700

    comparing learning curve of PCA per variable and PCA all

--- code/Python/feature_name_fiddling.py
+++ code/Python/feature_name_fiddling.py
@@ -4,34 +4,52 @@ import numpy as np
 import copy
 import sklearn.preprocessing
 import sklearn.decomposition
-
 from pprint import pprint
+import xgboost as xgb
 
+import machine_learning as ml
+
+
+CLASSIFIERS = {"xgboost": xgb.XGBClassifier(n_estimators=50)}
 
 
 def main():
     var_dict = extract_variable()
     df_dict, target_df, test_id_df = seprate_df_by_variable(var_dict)
-    df_dict = PCA_analysis(df_dict)
-    for df in df_dict.itervalues():
-        print df.shape
+    df_dict = PCA_analysis_per_variable(df_dict)
+    df_all, target_all = PCA_analysis_all()
+    print "PCA per varialbe"
+    ml.learning_curve(CLASSIFIERS, df_dict['train'], target_df)
+    print "PCA all"
+    ml.learning_curve(CLASSIFIERS, df_all, target_all)
 
 
-
-def PCA_analysis(df_dict):
+def PCA_analysis_per_variable(df_dict):
     new_df_dict = {"train": pd.DataFrame(), "test": pd.DataFrame()}
     for var_name, sub_df in df_dict.iteritems():
         scaler = sklearn.preprocessing.StandardScaler()
         X_train = scaler.fit_transform(sub_df["train"])
         X_test = scaler.fit_transform(sub_df["test"])
+        scaled_dict = {"train": X_train, "test": X_test}
         pca = sklearn.decomposition.PCA(n_components=0.999)
         pca.fit(X_train)
-        X_train_new = pd.DataFrame(data=pca.transform(X_train))
-        X_test_new = pd.DataFrame(data=pca.transform(X_test))
         accumulated_ratio = pca.explained_variance_ratio_.cumsum()
-        new_df_dict["train"] = pd.concat([new_df_dict["train"], X_train_new], axis="col")
-        new_df_dict["test"] = pd.concat([new_df_dict["test"], X_test_new], axis="col")
+        for data_type in ["train", "test"]:
+            X_new = pd.DataFrame(data=pca.transform(scaled_dict[data_type]))
+            new_df_dict[data_type] = pd.concat([new_df_dict[data_type], X_new], axis="col")
+            new_columns = range(len(new_df_dict[data_type].columns))
+            new_df_dict[data_type].columns = new_columns
     return new_df_dict
+
+def PCA_analysis_all():
+    df = pd.read_csv("../../data/train_4-26.csv")
+    df_target = df['TARGET']
+    df.drop('TARGET', axis=1, inplace=True)
+    scaler = sklearn.preprocessing.StandardScaler()
+    X = scaler.fit_transform(df)
+    pca = sklearn.decomposition.PCA(n_components=0.99)
+    return pd.DataFrame(data=pca.fit_transform(X)), df_target
+
 
 
 
--- code/Python/playground.py
+++ code/Python/playground.py
@@ -3,6 +3,14 @@ import numpy as np
 import pickle
 import sklearn.preprocessing
 
-df = pd.read_csv("../../data/train.csv")
 
-print pd.concat([df, pd.DataFrame()])
+
+df = pd.DataFrame(np.random.rand(4,4))
+
+print df
+print df.columns
+
+new_names = np.array(range(len(df.columns))).cumsum()
+
+df.columns = new_names
+print df
